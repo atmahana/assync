5916db237890a79f36159472f9b09baa01e69b8f
Adham <zubairadham.ar@gmail.com>
2023-09-12 21:55:28 +0800

    refactor: add loader toast when posts validating

--- components/posts/PostFeed.tsx
+++ components/posts/PostFeed.tsx
@@ -2,26 +2,21 @@ import usePosts from "@/hooks/post/usePosts";
 
 import PostItem from "./PostItem";
 import Skeleton from "../Skeleton";
-import { useEffect } from "react";
+import { HashLoader } from "react-spinners";
 
 interface PostFeedProps {
   userId?: string;
 }
 
 const PostFeed: React.FC<PostFeedProps> = ({ userId }) => {
-  const {
-    data: posts = [],
-    isLoading,
-    isValidating,
-    mutate,
-  } = usePosts(userId);
+  const { data: posts = [], isLoading, isValidating } = usePosts(userId);
 
   return (
-    <div className="pb-16 sm:pb-0 mx-3 sm:mx-0">
+    <div className="pb-16 sm:pb-0 mx-3 sm:mx-0 relative">
       {isValidating ? (
-        <p className="text-center text-lg font-semibold uppercase">
-          is validating
-        </p>
+        <div className="bg-background border border-border p-3 rounded-full w-fit fixed top-24 left-0 right-0 my-0 mx-auto drop-shadow-md">
+          <HashLoader color="#8b5cf6" size={20} />
+        </div>
       ) : null}
       {isLoading ? <Skeleton length={4} /> : null}
       {posts.map((post: Record<string, any>) => (
